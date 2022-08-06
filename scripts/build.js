3143406e16e8ea59c0ffea195babbdbf530862a1
Alan Zou <itisalanlife@gmail.com>
2022-08-06 14:28:44 +0800

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,13 @@
 const fs = require('fs');
-const path = require('path');
 
-const { spawnSync } = require('child_process');
+const { execSync } = require('child_process');
 
-spawnSync('npx tailwindcss -i ./src/styles.css -o ./dist/styles.css');
+execSync('npx tailwindcss -i ./src/styles.css -o ./dist/styles.css');
 
 const indexContent = fs.readFileSync('./src/index.html', 'utf8');
 
 fs.writeFileSync(
-  path.join(__dirname, '../dist/index.html'),
+  './dist/index.html',
   indexContent.replace(
     `<link href="/dist/styles.css" rel="stylesheet" />`,
     '<link href="./styles.css" rel="stylesheet" />',
